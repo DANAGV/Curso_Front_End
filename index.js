e8d1dbc350b4d905df108797868d1638d304e1c2
DANAGV <garvi.dana@gmail.com>
2020-10-04 23:35:27 -0500

    funciones parte 1

--- index.js
+++ index.js
@@ -1,55 +1,46 @@
-//* some()  verifica si los item pasa por una prueba
-//regresa true cuando lo uno lo cumple
-//sino false
-array = [12,2,52,1,2]
-position=array.some((age)=>age >= 18)
+//!FUNCIONES
+//**como se declaran las funciones */
+/*function("hola")
+function nameFuncion(atributos){
+    //*codigo de la funcion
+}
 
-//* shift()   elimina el primer elemento, pero cambia el array original y devuelve el elemento eliminado
-let deleteItem= array.shift()
-console.log(`Eliminando el primer elemento que es ${deleteItem} y ahora queda ${array}`)
+//*arrow funtions
+    //menos codigo
+    //la referencia THIS
+const nameFuncion = () =>{
+    //*todo el codigo de esta funcion
+}
+*/
 
-//*unshith  agrega elemento al inicio y devuelve una longitud
-let putItem= array.unshift(33)
-console.log(`agregando el primer elemento y ahora mi tamaño es ${putItem} y ahora queda ${array}`)
+//*FUNCION COOLBACK
+function sayHello(greet = () =>{
+    console.log('Hello')
+}){
+    let hello = true
+    hello ? greet() : null
+}
+sayHello() //llamar la funcion para que se muestre
 
-//*pop elimina elemento
-deleteItem = array.pop()
-console.log(deleteItem)
+//operaciones
+function suma(num,num2){
+    return num + num2
+}
+console.log(suma(2,2))
 
-//*push agrega elemento
-array.push(4)
-console.log(array)
+//*arrow funtions
+//cuando no se pone parentesis no se ejecuta la funcion sino hace referencia a ella
+//sin parentesis muestra toda la funcion
+//con parentesis muestra el resultadp de la operacion
+const sum1 = (num,num2) => num + num2
+console.log(sum1(3,3))
 
-//*slice()
-//parametros de donde empieza a donde termina
-newArray = array.slice(1,4)
-console.log(newArray)
+const printValue = value => console.log(value)
+printValue('Hola a todos')
 
-//*splice()
-//elimana o agrega elementos de un array
-    //para elimanar parametro =! de 0
-    //para agregar se pone un tercer elemento
-//parametos (posicion a borrar,item a eliminar)
-
-array.splice(2,1)
-console.log(array)
-
-//elimina posicion 2 y agrega hola
-array.splice(2,1,'hola')
-console.log(array)
-
-//*icludes()
-//si existe en el array true ,si no existe false
-let hasHola = array.includes('Dana')
-console.log(hasHola)
-
-//*sort() ordena los items
-//ordena el array alfabeticamente
-let alphabetArr = ['a','h','b','f','c']
-alphabetArr.sort()
-console.log(alphabetArr)
-
-//para hacerlo numericamente se agrega una funcion
-let array4=[20,3,24,7,14,1,5]
-array4.sort((a,b)=>a-b)
-console.log(array4)
+//si es mas de una instruccion son necesario las llaves
+const printValue2 = value => {
+    value = value.toUpperCase()
+    console.log(value)
+}
+printValue2('Hola a Todos')
