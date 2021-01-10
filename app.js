99d7be002d9548ac099c7dc72d803beef099908a
DANAGV <garvi.dana@gmail.com>
2021-01-09 22:52:37 -0600

    fetch

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-const helpers = require('./operations')
-const operations = require('./operations')
-console.log ('Hola mundo')
+fetch('https://rickandmortyapi.com/api/character/?page=19')
+    .then(response => {
+        console.log(response) //obtiene toda la respuesta y parciarla
+        return response.json()//para solicitar mas cosas se debe retornar
+        
+    })
+    .then(data => console.log(data))//se obtiene los datos porque se anidad con la primera
+    .catch (err => console.log(err))
+    
+/*fetch('https://rickandmortyapi.com/api/character/?page=19')
+    .then(response => response.json()
+    .then(data => console.log(data));*/
 
-/*function sum (a, b){
-    return a + b     SE PASA A HELPERS
+/*
+//*asyng await
+//async antes de la palabra reservada function   
+async function getCharacters () {
+    const response = await (await fetch('https://rickandmortyapi.com/api/character/?page=19')).json()
+    //const data = await response.json()   lo correcto seria separarlo
+    console.log(response)
 }*/
 
-const totalSum = operations.sum(2, 2)
-const totalRes = operations.resta(4,2)
+//!arrow funtion 
+//guarda una funcion anonima async es por funcion anoma que se iguala en una constante
 
-console.log('TOTAL SUM: ', totalSum)
-console.log('TOTAL RES: ', totalRes)
+const getCharacters = async () => {
+    try{//lo que quiero que la promesa se cumple
+        const response = await (await fetch('https://rickandmortyapi.com/api/character/?page=19')).json()
+        console.log(response)
+    }catch(error){//el error si no se cumple try
+        console.log(err)
+    }
+}
+
+getCharacters()
